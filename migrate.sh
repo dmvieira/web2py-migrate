1fa08efe8732f28fc962401d268f016d4c942235
Diogo <diogo.mvieira@labsynapse.com>
2014-08-29 00:32:45 -0300

    now migrates just use before and after migrates once and migrates changed folder

--- migrate.sh
+++ migrate.sh
@@ -29,10 +29,15 @@ make_migrate(){
 #create web2py paths
 mkdir $DATABASES $APP_PATH/sessions $APP_PATH/errors || true;
 
+#default do not re send to version control
+sendit=0;
 echo "making database backup";
 rds_backup || exit 10;
 echo "executing pre-migrate sqls";
-rds_import pre-migrate.sql || true;
+if [ -s "sql/before-${BRANCH}.sql" ];
+then
+    rds_import sql/before-${BRANCH}.sql && sendit=1 || exit 1;
+fi
 echo "making .tables with old db.py and fake_migrate if tag exists";
 #if tag dont exists then just make first migrate
 if vc_check_tag;
@@ -57,11 +62,22 @@ vc_status;
 cd ${DIR};
 make_migrate;
 echo "executing pos-migrate sqls";
-rds_import pos-migrate.sql || true;
+if [ -s "sql/after-${BRANCH}.sql" ];
+then
+    rds_import sql/after-${BRANCH}.sql && sendit=1 || exit 1;
+fi
 export MIGRATE_ENV=0;
 #last modifications with databases
-rds_finish;
+rds_finish || echo "error cleaning test server, please use finish by hand";
 echo "redefining tag"
-vc_remove_tag || true;
+vc_remove_tag || "error removing tag";
 vc_add_tag || echo "error adding tag";
+#send to version control again if did before and after migrates
+if [ $sendit -eq 1 ];
+then
+    > sql/before-${BRANCH}.sql || echo "error cleaning before migrate";
+    > sql/after-${BRANCH}.sql || echo "error cleaning after migrate";
+    vc_send || echo "error sending again to vc without after and before migrates";
+fi
+
 echo "migrate ok!";
